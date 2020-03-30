ce699d0718574841390d26c5db26e7b8aee72895
Tegar Bhakti <tegar.bhakti@students.amikom.ac.id>
2020-03-30 18:26:03 +0700

    second commit

---
+++ TheCoffeMechine/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace TheCoffeMechine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            CoffePowder coffe = new CoffePowder(100);
+            WaterGalon water = new WaterGalon(1000);
+            CoffeMechine yummycoffe = new CoffeMechine(coffe, water);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+            String result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+            result = yummycoffe.makeEsspresso();
+            Console.WriteLine("result " + result);
+
+            Console.WriteLine("check " + yummycoffe.checkAvailability());
+        }
+    }
+}
